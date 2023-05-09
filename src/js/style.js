4ae0abf2e3dfadd6753b7ff2a97b43349e7ef7ae
David Ferreira da Costa <120753154+Dlord250@users.noreply.github.com>
2023-05-08 22:11:11 -0300

    Add files via upload

---
+++ src/js/style.js
@@ -0,0 +1,26 @@
+window.sr = ScrollReveal ({reset: true});
+
+sr.reveal('.metalurgica', {duration : 1500});
+
+sr.reveal('.img1 img, .img2 img, .img3 img', {
+    duration: 2000,
+    rotate: {x: 100, }
+});
+
+sr.reveal('.img1, .img2, .img3', {
+    duration: 3000,
+    
+});
+
+sr.reveal('.img1 p, .img2 p, .img3 p', {
+    duration: 6000,
+    
+});
+
+sr.reveal('.img1:after, .img2:after, .img3:after,', {
+    duration: 3000,
+    
+});
+
+
+
